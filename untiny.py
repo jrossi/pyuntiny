a44f39609ae5e7d68b01ace680d4a826fc40a6f1
Jeremy Rossi <jeremy@jeremyrossi.com>
2009-08-12 13:35:08 -0400

    Quick and works for me

---
+++ untiny.py
@@ -0,0 +1,48 @@
+
+import urllib2
+try:
+    import json
+except ImportError:
+    import simplejson as json
+
+class UntinyError(Exception):
+    pass
+
+
+class untiny(object):
+    def __init__(self, url_base="http://untiny.me"):
+        self._url_base = url_base
+        self._urllib = urllib2
+
+    def _GetOpener(self, url):
+        return self._urllib.build_opener()
+
+    def _GetUrl(self, url):
+        opener = self._urllib.build_opener()
+        responce = opener.open(url).read()
+        opener.close()
+        data = json.loads(responce)
+        return data
+
+    def GetFullUrl(self, url):
+        data = self._GetUrl("%s/api/1.0/extract?format=json&url=%s"%(self._url_base, url))
+        if data.has_key("error"):
+            raise UntingError(str(data['error']))
+        elif data.has_key("org_url"):
+            return data["org_url"]
+        else:
+            raise UntinyError(u"Unknown responce from server: %s"%(str(data)))
+
+    def GetServices(self):
+        data = self._GetUrl("%s/api/1.0/services?format=json"%(self._url_base))
+        if len(data) > 0:
+            return data.keys()
+        else:
+            raise UntinyError(u"Unknown responce from server: %s"%(str(data)))
+
+
+
+                
+x = untiny()
+print x.GetServices()
+print x.GetFullUrl("http://tiny.pl/htk")
