bd1c1b9a0d00715e32dc7ca3098f7e55c2f4d968
Jeremy Rossi <jeremy@jeremyrossi.com>
2009-08-23 13:36:10 -0400

    clean up some issues and add some time util methods

--- untiny.py
+++ untiny.py
@@ -37,6 +37,7 @@ class untiny(object):
     def __init__(self, url_base="http://untiny.me"):
         self._url_base = url_base
         self._urllib = urllib2
+        self._services = []
 
     def _GetUrl(self, url):
         opener = self._urllib.build_opener()
@@ -45,18 +46,28 @@ class untiny(object):
         data = json.loads(responce)
         return data
 
+    def SupportedUrl(self, url):
+        svc = self.GetServices()
+        for i in svc:
+            if i in url:
+                return True
+        return False
+
     def GetFullUrl(self, url):
         data = self._GetUrl("%s/api/1.0/extract?format=json&url=%s"%(self._url_base, url))
         if data.has_key("error"):
-            raise UntingError(str(data['error']))
+            raise UntinyError(str(data['error']))
         elif data.has_key("org_url"):
             return data["org_url"]
         else:
             raise UntinyError(u"Unknown responce from server: %s"%(str(data)))
 
     def GetServices(self):
-        data = self._GetUrl("%s/api/1.0/services?format=json"%(self._url_base))
-        if len(data) > 0:
-            return data.keys()
-        else:
-            raise UntinyError(u"Unknown responce from server: %s"%(str(data)))
+        if not self._services:
+            data = self._GetUrl("%s/api/1.0/services?format=json"%(self._url_base))
+            if len(data) > 0:
+                return data.keys()
+            else:
+                raise UntinyError(u"Unknown responce from server: %s"%(str(data)))
+            self._services = data
+        return self._services
